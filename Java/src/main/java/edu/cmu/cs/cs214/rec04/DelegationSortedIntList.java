8ac9e67bb27bd95b82e4ac290a6e62dc9742aa33
rutone <zaga8037@gmail.com>
2025-04-21 13:17:46 +0800

    lab6 duusav

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,50 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList {
+    private final SortedIntList sortedList; // Delegate object
+    private int totalAdded; // Tracks the total number of add operations attempted
 
+    public DelegationSortedIntList() {
+        this.sortedList = new SortedIntList(); // Initialize the delegate object
+        this.totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int value) {
+        totalAdded++; // Increment the counter for each add attempt
+        return sortedList.add(value); // Delegate the add operation
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i)); // Use the overridden add method to track additions
+        }
+        return true;
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedList.get(index); // Delegate the get operation
+    }
+
+    @Override
+    public boolean remove(int value) {
+        return sortedList.remove(value); // Delegate the remove operation
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedList.removeAll(list); // Delegate the removeAll operation
+    }
+
+    @Override
+    public int size() {
+        return sortedList.size(); // Delegate the size operation
+    }
+
+    public int getTotalAdded() {
+        return totalAdded; // Return the total number of add operations
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,29 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
+    private int totalAdded; // Tracks the total number of add operations attempted
 
+    public InheritanceSortedIntList() {
+        super(); // Call the constructor of SortedIntList
+        totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int value) {
+        totalAdded++; // Increment the counter for each add attempt
+        return super.add(value); // Call the parent class's add method
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i)); // Use the overridden add method to track additions
+        }
+        return true;
+    }
+
+    public int getTotalAdded() {
+        return totalAdded; // Return the total number of add operations
+    }
 }
